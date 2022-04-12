eed0941245651fada99470d84c71edc1afa98a3f
정주헌 <cherish8513@naver.com>
2022-04-12 20:51:33 +0900

    feat: 글로벌 exception handler 추가

---
+++ shop/core/src/main/java/com/carver/aop/aspect/PointCuts.java
@@ -0,0 +1,26 @@
+package com.carver.aop.aspect;
+
+import org.aspectj.lang.annotation.Pointcut;
+
+public class PointCuts {
+
+  @Pointcut("execution(* com.carver..*(..))")
+  private void allSystem() {
+  }
+
+  @Pointcut("execution(* *..*Service.*(..))")
+  private void allService() {
+  }
+
+  @Pointcut("execution(* *..*Repository.*(..))")
+  private void allRepository() {
+  }
+
+  @Pointcut("execution(* *..*Controller.*(..))")
+  private void allController() {
+  }
+
+  @Pointcut("allSystem() && (allService() || allRepository() || allController())")
+  public void allAccess() {
+  }
+}
