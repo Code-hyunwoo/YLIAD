c40c234acc6e8e841cc8c951d462d13e847c7732
이현우 <chichyun92@naver.com>
2022-05-04 06:02:11 +0900

    S06P31A308-83 feat: Calendar, Emotion

--- frontend/src/routes/DayDiary.tsx
+++ frontend/src/routes/DayDiary.tsx
@@ -9,6 +9,7 @@ function DayDiary(){
             <h1>날짜별 일기</h1>
 
             <Link to="/lobby"><button> 로비로 이동 </button></Link>
+            <Link to="/calendar"><button> 달력으로 이동 </button></Link>
         </div>
     );
 }
