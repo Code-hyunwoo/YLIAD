ad99119337d59cb2bcf1af28babe17ba1b9324d1
김혜란 <ss-android@naver.com>
2022-05-03 23:28:49 +0900

    S06P31A308-79 feat: Nav

--- frontend/src/routes/VoiceDiary.tsx
+++ frontend/src/routes/VoiceDiary.tsx
@@ -1,15 +1,41 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Navbar from "../components/layout/Navbar";
 import base from "./Base.module.css"
+import Styles from "./Voice.module.css"
 
 
 function VoiceDiary(){
 
-    return (
-        <div className={base.container}> 
-            <h1>음성 일기</h1>
+    //recode
+    const [recoding, setRecoding] = useState<boolean>(false);
+    const getRecoding = () => {
+        setRecoding(!recoding);
+    }
 
-            <Link to="/lobby"><button> 로비로 이동 </button></Link>
-        </div>
+
+
+    return (
+        <>
+            <Navbar />
+            <div className={base.container}> 
+                {/* <h1>음성 일기</h1> */}
+
+                {/* <Link to="/lobby"><button> 로비로 이동 </button></Link> */}
+
+                <div className={Styles.time}>
+                    10 : 00
+                </div>
+                {/* <button className={Styles.recode} > </button> */}
+                {/* <button className={Styles.recode} id="recode" onClick={getRecoding} > </button> */}
+                <button className={ recoding? Styles.recoding : Styles.recode} onClick={getRecoding} > </button>
+                <div className={Styles.button}>
+                    <img className={Styles.save} src="https://img.icons8.com/ios-filled/32/FFFFFF/installing-updates--v1.png"/>
+                    <Link to="/selectdiary"><img className={Styles.back} src="https://img.icons8.com/office/30/FFFFFF/undo.png"/></Link>
+                </div>
+                <div className={Styles.info}> 녹음 시간은 최대 10분입니다. </div>
+            </div>
+        </>
     );
 }
 
