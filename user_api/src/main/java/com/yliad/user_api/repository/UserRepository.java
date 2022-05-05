ac1aa7bb8e9a3d88ef5fb388ebd481c01094001c
BaeJiHwan <bgh1906@naver.com>
2022-05-05 17:42:13 +0900

    S06P31A308-16 feat: user_api entity, repository 생성 및 테스트

---
+++ user_api/src/main/java/com/yliad/user_api/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.yliad.user_api.repository;
+
+import com.yliad.user_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
