12811170f9d285c7a1a7afd173261baf888c97f6
정주헌 <cherish8513@naver.com>
2022-05-09 11:00:01 +0900

    feat: api gateway project 생성

---
+++ api_gateway/src/main/kotlin/com/yliad/gateway/ApiGatewayApplication.kt
@@ -0,0 +1,21 @@
+package com.yliad.gateway
+
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.Bean
+
+@ConfigurationPropertiesScan
+@SpringBootApplication
+@EnableDiscoveryClient
+class ApiGatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiGatewayApplication>(*args)
+}
+
+@Bean
+fun httpTraceRepository() : HttpTraceRepository = InMemoryHttpTraceRepository()
---
+++ api_gateway/src/main/kotlin/com/yliad/gateway/config/JwtProperties.kt
@@ -0,0 +1,11 @@
+package com.yliad.gateway.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "token")
+data class JwtProperties (
+    val expirationTime : String,
+    val secret : String
+)
