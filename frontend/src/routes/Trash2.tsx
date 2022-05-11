5c96ddb696275233bd658752d62eeef2e809fccf
bruno <bruno95@naver.com>
2022-05-11 15:11:23 +0900

    S06P31A308-105 feat: trash fadeout animation done

--- frontend/src/routes/Trash2.tsx
+++ frontend/src/routes/Trash2.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import { BrowserView, MobileView } from "react-device-detect";
 import { Link } from "react-router-dom";
 import Navbar from "../components/layout/Navbar";
-import Stars2 from "../components/layout/Stars2";
 import base from "./Base.module.css";
 import styles from "./Trash2.module.css";
 
 function Trash2() {
+  const [text, setText] = useState<string>("");
+  const [isburned, setIsburned] = useState<boolean>(false);
+  function burn() {
+    setIsburned(true);
+    // setIsburned((prev) => !prev);
+    // console.log("clicked!", isburned);
+  }
+  function addTrash() {
+    setIsburned(false);
+    setText("");
+  }
   return (
     <>
       <Navbar />
@@ -43,12 +54,15 @@ function Trash2() {
               <div></div>
             </div>
           </div>
-          <p>
+          <p className={styles.inputP}>
             지우고 싶은 기억을 입력하고 불태우세요..
             <br />
             <textarea />
             <button>불태워 지우기</button>
             <span className={styles.authorP}>Burn it</span>
+            <Link to="/lobby">
+              <button> 로비로 이동 </button>
+            </Link>
           </p>
           <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
             <defs>
@@ -105,13 +119,29 @@ function Trash2() {
               <div></div>
             </div>
           </div>
-          <p>
-            지우고 싶은 기억을 입력하고 불태우세요..
-            <br />
-            <textarea />
-            <button>불태워 지우기</button>
-            <span className={styles.author}>Burn it</span>
-          </p>
+          <div className={styles.input}>
+            <p>
+              지우고 싶은 기억을
+              <br />
+              불태우세요
+            </p>
+            <textarea
+              id={styles.text}
+              name="textdiary"
+              value={text}
+              onChange={(ev: React.ChangeEvent<HTMLTextAreaElement>): void =>
+                setText(ev.target.value)
+              }
+              className={isburned ? styles.burn : undefined}
+            />
+            <button onClick={addTrash}>추가 작성하기</button>
+            <span className={styles.burnButton} onClick={burn}>
+              Burn it
+            </span>
+            <Link to="/lobby">
+              <button> 로비로 이동 </button>
+            </Link>
+          </div>
           <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
             <defs>
               <filter id="goo">
