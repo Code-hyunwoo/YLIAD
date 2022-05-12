8cd8fe35ded57b06b7be86014720fb3fd1700f0b
BaeJiHwan <bgh1906@naver.com>
2022-05-12 18:16:25 +0900

    S06P31A308-123 fix: entity 변경에 따른 기존 모듈 제거

--- user_api/src/main/java/com/yliad/font/repository/FontRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yliad.font.repository;
-
-import com.yliad.font.entity.Font;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FontRepository extends JpaRepository<Font, Long> {
-}
