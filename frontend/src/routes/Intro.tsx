34d86dfd07ba4fd591b48f2642f6ed5a0ed0492c
bruno <bruno95@naver.com>
2022-05-16 02:01:26 +0900

    feat: 사진추가 / 다운로드 페이지 임시내브바, 헤더 완성

--- frontend/src/routes/Intro.tsx
+++ frontend/src/routes/Intro.tsx
@@ -1,7 +1,39 @@
 import styles from "./Intro.module.css";
+import moon from "../assets/images/moon.png";
+import apk from "../assets/images/downloadapk.png";
+import moving from "../assets/images/movingad.gif";
 
 function Intro() {
-  return <>안녕 난 인트로</>;
+  return (
+    <div className={styles.wrapper}>
+      {/* Navbar */}
+      <div className={styles.navbar}>
+        <div className={styles.icon}>
+          <img src={moon} alt="moon" />
+          YLIAD
+        </div>
+        <ul className={styles.menu}>
+          <li>FEATURES</li>
+          <li>ABOUT US</li>
+        </ul>
+      </div>
+      {/* 서비스 소개 - 제목 */}
+      <div className={styles.header}>
+        <p>
+          AI Voice Diary App for Web, Android
+          <br />
+        </p>
+        <span>
+          인공지능 음성 다이어리 YLIAD로 하루를 아름답게 마무리하세요!
+        </span>
+      </div>
+      {/* 서비스 다운로드 / 구독 */}
+      <div className={styles.downsubs}>
+        <img src={apk} alt="apkLink" />
+      </div>
+      <img src={moving} alt="movingad" id={styles.movingAd} />
+    </div>
+  );
 }
 
 export default Intro;
