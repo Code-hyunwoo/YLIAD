1d07e1e91d8091080accacd8dc7c809d426c0839
정주헌 <cherish8513@naver.com>
2022-05-16 10:11:53 +0900

    fix: cors prefilight 설정 수정

--- api_gateway/src/main/kotlin/com/yliad/gateway/config/CorsFilter.java
+++ api_gateway/src/main/kotlin/com/yliad/gateway/config/CorsFilter.java
@@ -1,26 +1,18 @@
 package com.yliad.gateway.config;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+
 import org.springframework.cloud.gateway.config.GlobalCorsProperties;
 import org.springframework.cloud.gateway.handler.FilteringWebHandler;
 import org.springframework.cloud.gateway.handler.RoutePredicateHandlerMapping;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
-import org.springframework.web.cors.CorsConfiguration;
 
-@ConfigurationProperties("spring.cloud.gateway.globalcors")
+@Configuration
 public class CorsFilter {
 
-  private final Map<String, CorsConfiguration> corsConfigurations = new LinkedHashMap<>();
-
-  public Map<String, CorsConfiguration> getCorsConfigurations() {
-    return corsConfigurations;
-  }
-  
   @Bean
   @Primary
   public RoutePredicateHandlerMapping passCorsRoutePredicateHandlerMapping(
