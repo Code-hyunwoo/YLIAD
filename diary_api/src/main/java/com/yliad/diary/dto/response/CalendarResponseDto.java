b44032130ba250b651b7032e589458476567983d
BaeJiHwan <bgh1906@naver.com>
2022-05-16 13:36:07 +0900

    S06P31A308-63 fix: diary Notnull 추가

--- diary_api/src/main/java/com/yliad/diary/dto/response/CalendarResponseDto.java
+++ diary_api/src/main/java/com/yliad/diary/dto/response/CalendarResponseDto.java
@@ -1,6 +1,7 @@
 package com.yliad.diary.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sun.istack.NotNull;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,10 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class CalendarResponseDto {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd")
-    LocalDateTime day;
-    String emotion;
+    @NotNull
+    private LocalDateTime day;
+    @NotNull
+    private String emotion;
 
     @Builder
     public CalendarResponseDto(LocalDateTime day, String emotion) {
