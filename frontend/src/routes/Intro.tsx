d8c7673b0bd38acb93e537abde8c6ee5439f262c
bruno <bruno95@naver.com>
2022-05-17 19:15:35 +0900

    S06P31A308-131 feat: 다운로드 페이지 MobileView Init
    Former-commit-id: cecf3ff3a12867000608c68a0d466e19043dd209

--- frontend/src/routes/Intro.tsx
+++ frontend/src/routes/Intro.tsx
@@ -11,7 +11,7 @@ import { useState } from "react";
 import { styled } from "@mui/material/styles";
 import Tooltip, { TooltipProps, tooltipClasses } from "@mui/material/Tooltip";
 import Zoom from "@mui/material/Zoom";
-import { BrowserView } from "react-device-detect";
+import { BrowserView, MobileView } from "react-device-detect";
 import { useNavigate } from "react-router-dom";
 
 function Intro() {
@@ -88,7 +88,7 @@ function Intro() {
                 <li>FEATURES</li>
               </ul>
             </div>
-            <div style={{ display: "flex" }}>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
               {/* 아드를 옆에 일정 간격으로 배치하기 위한 flex 설정 */}
               <div>
                 {/* 서비스 소개 - 제목 */}
@@ -150,7 +150,7 @@ function Intro() {
                   </div>
                 </div>
               </div>
-              <img src={moving} alt="movingad" id={styles.movingAd} />
+              <img src={moving} alt="movingad" />
             </div>
           </div>
         </section>
@@ -224,6 +224,159 @@ function Intro() {
           </div>
         </section>
       </BrowserView>
+
+      <MobileView>
+        {/* 섹션 1 - 내브바 & 다운링크 & 구독 */}
+        <section className={styles.section1M}>
+          <div className={styles.wrapper_introM}>
+            <ToastContainer className={styles.toastM} />
+            {/* 네비게이션 바 */}
+            <div className={styles.navbarM}>
+              <div className={styles.iconM}>
+                <img src={moon} alt="moon" />
+                YLIAD
+              </div>
+              <ul className={styles.menuM}>
+                <li>INTRO VIDEO</li>
+                <li>FEATURES</li>
+              </ul>
+            </div>
+            <div style={{ display: "flex" }}>
+              {/* 아드를 옆에 일정 간격으로 배치하기 위한 flex 설정 */}
+              <div>
+                {/* 서비스 소개 - 제목 */}
+                <div className={styles.headerM}>
+                  <p>
+                    AI Voice Diary App for Web, Android
+                    <br />
+                  </p>
+                  <span>
+                    인공지능 음성 다이어리 YLIAD로 하루를 아름답게 마무리하세요!
+                  </span>
+                </div>
+                {/* 서비스 다운로드 / 구독 */}
+                <div className={styles.downsubsM}>
+                  <LightTooltip
+                    placement="top"
+                    title="WEB으로 시작하기"
+                    arrow
+                    TransitionComponent={Zoom}
+                  >
+                    <img
+                      src={web}
+                      alt="webVersion"
+                      id={styles.web_availableM}
+                      onClick={goWebVersion}
+                    />
+                  </LightTooltip>
+                  <a
+                    href="https://bit.ly/DownloadYLIAD"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src={apk}
+                      alt="apkLink"
+                      id={styles.android_availableM}
+                    />
+                  </a>
+                  <LightTooltip
+                    placement="top"
+                    title="iOS 버전도 곧 출시될 예정이에요!"
+                    arrow
+                    TransitionComponent={Zoom}
+                  >
+                    <img src={ipa} alt="ipaDisabled" id={styles.ios_NAM} />
+                  </LightTooltip>
+                  <div className={styles.subscribeM}>
+                    <input
+                      type="email"
+                      placeholder="Your Email Address"
+                      value={email}
+                      onChange={getEmail}
+                    />
+                    <button onClick={subscribe}>Subscribe</button>
+                    <p style={{ fontSize: "20px" }}>
+                      구독하시면 YLIAD의 최신 업데이트 내용, 이벤트 소식을
+                      누구보다 빨리 받아보실 수 있습니다.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <img src={moving} alt="movingad" />
+            </div>
+          </div>
+        </section>
+
+        {/* 섹션 2 - UCC */}
+        <section className={styles.section2M}>
+          <div className={styles.wrapper_videoM}>
+            <p>Welcome to YLIAD</p>
+            <video src={ucc} itemType="video/mp4" controls loop></video>
+          </div>
+        </section>
+
+        {/* 섹션 3 - 서비스 기능 */}
+        <section className={styles.section3M}>
+          <div className={styles.wrapper_bodyM}>
+            <div className={styles.feature1M}>
+              <img src={read} alt="YLIAD1" />
+              <div className={styles.feature_textM}>
+                <p>YLIAD는 인공지능입니다.</p>
+                <span>
+                  인공지능 챗봇을 통해 힘든 마음을 치유할 수 있고,
+                  <br />
+                  감정분석도 해줍니다.
+                </span>
+              </div>
+            </div>
+            <div className={styles.feature2M}>
+              <img src={read} alt="YLIAD2" />
+              <div className={styles.feature_textM}>
+                <p>YLIAD는 감정쓰레기통입니다.</p>
+                <span>
+                  감정쓰레기통에서는
+                  <br />
+                  잊고 싶은 오늘의 기억을 버려두고 가세요.
+                </span>
+              </div>
+            </div>
+            <div className={styles.feature3M}>
+              <img src={read} alt="YLIAD3" />
+              <div className={styles.feature_textM}>
+                <p>
+                  아드는 당신보다
+                  <br />
+                  당신을 더 잘 알아요.
+                </p>
+                <span>
+                  인공지능 아드는
+                  <br />
+                  당신의 일기를 분석해 감정을 알려줍니다.
+                </span>
+              </div>
+            </div>
+            <div className={styles.feature4M}>
+              <img src={read} alt="YLIAD4" />
+              <div className={styles.feature_textM}>
+                <p>YLIAD는 감정쓰레기통입니다.</p>
+                <span>
+                  감정쓰레기통에서는
+                  <br />
+                  잊고 싶은 오늘의 기억을 버려두고 가세요.
+                </span>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* 섹션 4 - 푸터 */}
+        <section className={styles.section4M}>
+          <div className={styles.wrapper_footerM}>
+            <p>Copyright © 2022 YLIAD. All Rights Reserved.</p>
+          </div>
+        </section>
+      </MobileView>
     </>
   );
 }
