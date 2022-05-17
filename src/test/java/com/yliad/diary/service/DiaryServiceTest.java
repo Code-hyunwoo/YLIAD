3b50415bc39577835175cb8f95b9eb7e925bf058
BaeJiHwan <bgh1906@naver.com>
2022-05-17 23:33:28 +0900

    S06P31A308-150 fix: 경로수정

---
+++ src/test/java/com/yliad/diary/service/DiaryServiceTest.java
@@ -0,0 +1,14 @@
+package com.yliad.diary.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiaryServiceTest {
+
+
+    @Test
+    void getMyPage() {
+
+    }
+}
