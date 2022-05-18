b707394fc09218d24f7a0a26ad51c8e8d80b3375
BaeJiHwan <bgh1906@naver.com>
2022-05-19 03:22:18 +0900

    S06P31A308-150 fix: h2에서 mariadb로 변경되며 테이블 호출 값에 db이름 추가필요 변경

--- src/main/java/com/yliad/diary/repository/DiaryRepository.java
+++ src/main/java/com/yliad/diary/repository/DiaryRepository.java
@@ -10,29 +10,29 @@ import java.util.List;
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     @Query(nativeQuery = true, value = "SELECT count(diary_Date)\n" +
-            "FROM DIARY\n" +
+            "FROM yliad.diary\n" +
             "WHERE userid = :userid AND YEAR(diary_Date) = :year AND MONTH(diary_Date) = :month\n")
     Integer countDiaryMonth(@Param("userid") Long userid, @Param("year") int year, @Param("month") int month);
 
     @Query(nativeQuery = true, value = "SELECT count(diary_Date)\n" +
-            "FROM DIARY\n" +
+            "FROM yliad.diary\n" +
             "WHERE userid = :userid AND YEAR(diary_Date) = :year\n")
     Integer countDiaryYear(@Param("userid") Long userid, @Param("year") int year);
 
     @Query(nativeQuery = true, value = "SELECT count(diary_Date)\n" +
-            "FROM DIARY\n" +
+            "FROM yliad.diary\n" +
             "WHERE userid = :userid\n")
     Integer countDiaryTotal(@Param("userid") Long userid);
 
     @Query(nativeQuery = true, value ="SELECT emotion\n" +
-            "FROM DIARY\n" +
+            "FROM yliad.diary\n" +
             "WHERE userid = :userid AND YEAR(diary_Date) = :year AND MONTH(diary_Date) = :month\n" +
             "GROUP BY emotion\n" +
             "ORDER BY emotion")
     List<String> countMonthEmotion(@Param("userid") Long userid, @Param("year") int year, @Param("month") int month);
 
     @Query(nativeQuery = true, value = "SELECT count(emotion)\n" +
-            "FROM DIARY\n" +
+            "FROM yliad.diary\n" +
             "WHERE userid = :userid AND YEAR(diary_Date) = :year AND MONTH(diary_Date) = :month\n" +
             "GROUP BY emotion\n" +
             "ORDER BY emotion")
