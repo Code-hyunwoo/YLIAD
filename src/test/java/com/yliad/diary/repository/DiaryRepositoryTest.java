009a844e4cc5f09de08b29cf9a01a39b8fcc7d00
BaeJiHwan <bgh1906@naver.com>
2022-05-18 18:55:00 +0900

    S06P31A308-150 fix: test 주석처리

--- src/test/java/com/yliad/diary/repository/DiaryRepositoryTest.java
+++ src/test/java/com/yliad/diary/repository/DiaryRepositoryTest.java
@@ -12,122 +12,122 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class DiaryRepositoryTest {
     @Autowired
     DiaryRepository diaryRepository;
-
-    @Test
-    void QueryTest(){
-        Diary diary = Diary.builder()
-                .emotion("a")
-                .content("1")
-                .build();
-        Diary diary1 = Diary.builder()
-                .emotion("a")
-                .content("1")
-                .build();
-        Diary diary2 = Diary.builder()
-                .emotion("b")
-                .content("1")
-                .build();
-        Diary diary3 = Diary.builder()
-                .emotion("c")
-//                .diaryDate(LocalDateTime.parse("2022-05-28 15:18"))
-                .content("1")
-                .build();
-
-        //isnull(0)
-        diaryRepository.save(diary);
-        diaryRepository.save(diary1);
-        diaryRepository.save(diary2);
-        diaryRepository.save(diary3);
-
-        List<Integer> list = diaryRepository.countMonthEmotionCount(0L, 2022, 5);
-
-//        List<Integer> list = diaryRepository.countEmotion();
-        for(int i = 0; i < list.size(); i++){
-            System.out.println(list.get(i));
-        }
-        diaryRepository.delete(diary);
-        diaryRepository.delete(diary1);
-        diaryRepository.delete(diary2);
-        diaryRepository.delete(diary3);
-    }
-
-
-    @Test
-    void countEmotionTest(){
-        Diary diary = Diary.builder()
-                .emotion("love")
-                .content("1")
-                .build();
-        Diary diary1 = Diary.builder()
-                .emotion("love")
-                .content("1")
-                .build();
-        Diary diary2 = Diary.builder()
-                .emotion("joy")
-                .content("1")
-                .build();
-        Diary diary3 = Diary.builder()
-                .emotion("anger")
-                .content("1")
-                .build();
-
-        diaryRepository.findById(1L);
-
-        //isnull(0)
-        diaryRepository.save(diary);
-        diaryRepository.save(diary1);
-        diaryRepository.save(diary2);
-        diaryRepository.save(diary3);
-
-        List<String> list2 = diaryRepository.countMonthEmotion(0L,2022, 5);
-        List<Integer> list = diaryRepository.countMonthEmotionCount(0L,2022, 5);
-
-//        List<Integer> list = diaryRepository.countEmotion();
-        for(int i = 0; i < list.size(); i++){
-            System.out.println(list2.get(i));
-            System.out.println(list.get(i));
-        }
-
-    }
-
-    @Test
-    void countMonthEmotionTest(){
-
-        Diary diary = Diary.builder()
-                .emotion("love")
-                .content("1")
-                .build();
-        Diary diary1 = Diary.builder()
-                .emotion("love")
-                .content("1")
-                .build();
-        Diary diary2 = Diary.builder()
-                .emotion("joy")
-                .content("1")
-                .build();
-        Diary diary3 = Diary.builder()
-                .emotion("anger")
-//                .diaryDate(LocalDateTime.parse("2022-05-28 15:18"))
-                .content("1")
-                .build();
-
-        diaryRepository.findById(1L);
-
-        //isnull(0)
-        diaryRepository.save(diary);
-        diaryRepository.save(diary1);
-        diaryRepository.save(diary2);
-        diaryRepository.save(diary3);
-
-        List<String> list2 = diaryRepository.countMonthEmotion(0L,2022, 5);
-        List<Integer> list = diaryRepository.countMonthEmotionCount(0L,2022, 5);
-
-        int year = diaryRepository.countDiaryYear(0L, 2022);
-        int month = diaryRepository.countDiaryMonth(0L, 2022, 5);
-        int total = diaryRepository.countDiaryTotal(0L);
-
-        System.out.println(year);
-        System.out.println(month);
-        System.out.println(total);
-        }
+//
+//    @Test
+//    void QueryTest(){
+//        Diary diary = Diary.builder()
+//                .emotion("a")
+//                .content("1")
+//                .build();
+//        Diary diary1 = Diary.builder()
+//                .emotion("a")
+//                .content("1")
+//                .build();
+//        Diary diary2 = Diary.builder()
+//                .emotion("b")
+//                .content("1")
+//                .build();
+//        Diary diary3 = Diary.builder()
+//                .emotion("c")
+////                .diaryDate(LocalDateTime.parse("2022-05-28 15:18"))
+//                .content("1")
+//                .build();
+//
+//        //isnull(0)
+//        diaryRepository.save(diary);
+//        diaryRepository.save(diary1);
+//        diaryRepository.save(diary2);
+//        diaryRepository.save(diary3);
+//
+//        List<Integer> list = diaryRepository.countMonthEmotionCount(0L, 2022, 5);
+//
+////        List<Integer> list = diaryRepository.countEmotion();
+//        for(int i = 0; i < list.size(); i++){
+//            System.out.println(list.get(i));
+//        }
+//        diaryRepository.delete(diary);
+//        diaryRepository.delete(diary1);
+//        diaryRepository.delete(diary2);
+//        diaryRepository.delete(diary3);
+//    }
+//
+//
+//    @Test
+//    void countEmotionTest(){
+//        Diary diary = Diary.builder()
+//                .emotion("love")
+//                .content("1")
+//                .build();
+//        Diary diary1 = Diary.builder()
+//                .emotion("love")
+//                .content("1")
+//                .build();
+//        Diary diary2 = Diary.builder()
+//                .emotion("joy")
+//                .content("1")
+//                .build();
+//        Diary diary3 = Diary.builder()
+//                .emotion("anger")
+//                .content("1")
+//                .build();
+//
+//        diaryRepository.findById(1L);
+//
+//        //isnull(0)
+//        diaryRepository.save(diary);
+//        diaryRepository.save(diary1);
+//        diaryRepository.save(diary2);
+//        diaryRepository.save(diary3);
+//
+//        List<String> list2 = diaryRepository.countMonthEmotion(0L,2022, 5);
+//        List<Integer> list = diaryRepository.countMonthEmotionCount(0L,2022, 5);
+//
+////        List<Integer> list = diaryRepository.countEmotion();
+//        for(int i = 0; i < list.size(); i++){
+//            System.out.println(list2.get(i));
+//            System.out.println(list.get(i));
+//        }
+//
+//    }
+//
+//    @Test
+//    void countMonthEmotionTest(){
+//
+//        Diary diary = Diary.builder()
+//                .emotion("love")
+//                .content("1")
+//                .build();
+//        Diary diary1 = Diary.builder()
+//                .emotion("love")
+//                .content("1")
+//                .build();
+//        Diary diary2 = Diary.builder()
+//                .emotion("joy")
+//                .content("1")
+//                .build();
+//        Diary diary3 = Diary.builder()
+//                .emotion("anger")
+////                .diaryDate(LocalDateTime.parse("2022-05-28 15:18"))
+//                .content("1")
+//                .build();
+//
+//        diaryRepository.findById(1L);
+//
+//        //isnull(0)
+//        diaryRepository.save(diary);
+//        diaryRepository.save(diary1);
+//        diaryRepository.save(diary2);
+//        diaryRepository.save(diary3);
+//
+//        List<String> list2 = diaryRepository.countMonthEmotion(0L,2022, 5);
+//        List<Integer> list = diaryRepository.countMonthEmotionCount(0L,2022, 5);
+//
+//        int year = diaryRepository.countDiaryYear(0L, 2022);
+//        int month = diaryRepository.countDiaryMonth(0L, 2022, 5);
+//        int total = diaryRepository.countDiaryTotal(0L);
+//
+//        System.out.println(year);
+//        System.out.println(month);
+//        System.out.println(total);
+//        }
 }
